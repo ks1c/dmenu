cbccd81d8f3989921b1094237acb5f2c80675489
ks1c <iamks1c@gmail.com>
2020-03-21 14:37:03 -0400

    add dracula colorscheme and change font size

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#f8f8f2", "#21222c" },
+	[SchemeSel] = { "#21222c", "#bd93f9" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
